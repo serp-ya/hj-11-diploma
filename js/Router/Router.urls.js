8887fe736b0aa71790c46e81bc1d8dd9fa861af8
serp-ya <sergei.kundryukov1989@gmail.com>
2017-12-05 09:25:34 +0300

    fix ssl conflict

--- js/Router/Router.urls.js
+++ js/Router/Router.urls.js
@@ -1,4 +1,4 @@
 const goodsRequestApiUrl = 'https://hj-diploma-backend.herokuapp.com/api/v1/goods';
 const goodsCountRequestApiUrl = 'https://hj-diploma-backend.herokuapp.com/api/v1/goods?count=true';
 const userCartRequestApiUrl = 'https://hj-diploma-backend.herokuapp.com/api/v1/carts';
-const webSocketUrl = 'ws://hj-diploma-backend.herokuapp.com';
+const webSocketUrl = 'wss://hj-diploma-backend.herokuapp.com';
