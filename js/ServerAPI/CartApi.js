662874c373aca775c921be7f9a8a1dcd24eeee74
serp-ya <sergei.kundryukov1989@gmail.com>
2017-12-26 06:38:13 +0300

    Убрал лишнюю промисификацию из файла отвечающего за Model

--- js/ServerAPI/CartApi.js
+++ js/ServerAPI/CartApi.js
@@ -9,69 +9,23 @@ function addProduct(productId) {
   const requestConfig = Object.assign({}, requestJsonConfigs.post);
   requestConfig.body = JSON.stringify({'_id': productId});
 
-  return new Promise((done, fail) => {
-    fetch(userCartRequestApiUrl, requestConfig)
-      .then(res => {
-        if (200 < res.status || res.status > 299) {
-          throw new Error('Invalid request status code');
-        }
-
-        // window.dispatchEvent(window.updateCartCountEvent);
-        done(res);
-      })
-      .catch(error => fail(error));
-  });
+  return fetch(userCartRequestApiUrl, requestConfig);
 }
 
 function updateProductItemQuantity(productId, newQuantity) {
   const updateConfig = Object.assign({}, requestJsonConfigs.put);
   updateConfig.body = JSON.stringify({'_id': productId, count: newQuantity});
 
-  return new Promise((done, fail) => {
-    fetch(userCartRequestApiUrl, updateConfig)
-      .then(res => {
-        if (200 < res.status || res.status > 299) {
-          throw new Error(`Invalid status: ${res.status}`);
-        }
-
-        // window.dispatchEvent(window.updateCartCountEvent);
-        done(newQuantity);
-      })
-      .catch(error => fail(error));
-  });
+  return fetch(userCartRequestApiUrl, updateConfig);
 }
 
 function deleteProduct(productId) {
   const deleteConfig = Object.assign({}, requestJsonConfigs.delete);
   deleteConfig.body = JSON.stringify({'_id': productId});
 
-  return new Promise((done, fail) => {
-    fetch(userCartRequestApiUrl, deleteConfig)
-      .then(res => {
-        if (200 < res.status || res.status > 299) {
-          throw new Error(`Invalid status: ${res.status}`);
-        }
-
-        // window.dispatchEvent(window.updateCartCountEvent);
-        done(res);
-      })
-      .catch(error => fail(error));
-  });
+  return fetch(userCartRequestApiUrl, deleteConfig);
 }
 
 function updateCartCount() {
-  return new Promise((done, fail) => {
-    fetch(userCartRequestApiUrl + '?count=true', requestDefaultConfig)
-      .then(res => {
-        if (res.status === 404) {
-          throw new Error('Cart not found');
-
-        } else if (200 < res.status || res.status > 299) {
-          throw new Error('Invalid request status code');
-        }
-
-        done(res.json());
-      })
-      .catch(error => fail(error));
-  })
+  return fetch(userCartRequestApiUrl + '?count=true', requestDefaultConfig);
 }
